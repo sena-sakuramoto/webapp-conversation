0695b1d9fcf3c4d936573a304f6a254e7e05885b
sena-sakuramoto <s.sakuramoto@archi-prisma.co.jp>
2025-03-08 20:07:30 +0900

    Create BuildingCodeAdvisor.tsx

---
+++ app/components/BuildingCodeAdvisor.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function BuildingCodeAdvisor() {
+  // ここに私が提供したHTMLを適切にReact形式に変換したコードを貼り付け
+  return (
+    <div className="app-container">
+      {/* サイドバー部分 */}
+      <aside className="sidebar">
+        {/* ロゴコンテナ */}
+        <div className="logo-container">
+          <img src="/logo.svg" alt="建築法規アドバイザー" className="logo" />
+          <h1>建築法規<br />アドバイザー</h1>
+        </div>
+        
+        {/* 残りのコードも同様に */}
+      </aside>
+      
+      {/* メインコンテンツ */}
+      <main className="main-content">
+        {/* ... */}
+      </main>
+    </div>
+  );
+}
